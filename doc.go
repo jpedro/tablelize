af93cc7ef21ed4fdafc3a10b50a053aaab42734d
Pedro Barbosa <jpedro.barbosa@gmail.com>
2017-09-01 11:39:05 +0200

    Morph real-time infrastructures

---
+++ doc.go
@@ -0,0 +1,36 @@
+// # tablelize
+// 
+// Prints out the values of a table (array of arrays) of strings nicely align by
+// width.
+// 
+// 
+// ### Usage
+// 
+// ```go
+// package main
+// 
+// import (
+//   "github.com/jpedro/tablelize"
+// )
+// 
+// func main() {
+//   var data [][]string
+//   var row []string
+// 
+//   row = []string{"KEY", "VALUE"}
+//   data = append(data, row)
+// 
+//   row = []string{"char", "a"}
+//   data = append(data, row)
+// 
+//   row = []string{"medium", "Some text with content"}
+//   data = append(data, row)
+// 
+//   row = []string{"long", "And now for something very very loooong"}
+//   data = append(data, row)
+// 
+//   tablelize.Rows(data)
+// }
+// ```
+// 
+// Check https://github.com/jpedro/tablelize/tree/master/examples for examples.
