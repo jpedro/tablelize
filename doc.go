ccc71809139c6b86fb690a252b1e1fdf5186e104
Pedro Barbosa <jpedro.barbosa@gmail.com>
2019-10-29 06:51:02 +0100

    Never Run This Commit As Root

--- doc.go
+++ doc.go
@@ -1,35 +1,39 @@
-// Prints out the values of a table (array of arrays) of strings nicely align by
-// width. It also tries to align all numeric values to the right when all are
-// numeric.
-//
-//
-// Usage
-//
-// package main
-// 
-// import (
-//   "github.com/jpedro/tablelize"
-// )
-// 
-// func main() {
-//   var data [][]string
-// 
-//   data = append(data, []string{"KEY", "VALUE", "NUMBER", "ALMOST_NUMBER"})
-//   data = append(data, []string{"char", "a", "1", "1"})
-//   data = append(data, []string{"longer-key-name", "Some text", "-2", "2"})
-//   data = append(data, []string{"key", "And now for something completely different", "3", "3a"})
-// 
-//   tablelize.Rows(data)
-// }
-//
-// Output:
-//
-//   % go run examples/main.go
-//   KEY               VALUE                                        NUMBER   ALMOST_NUMBER
-//   char              a                                                 1   1
-//   longer-key-name   Some text                                        -2   2
-//   key               And now for something completely different        3   3a
+/*
+Prints out the values of a table (array of arrays) of strings nicely align by
+width. It also tries to align all numeric values to the right when all are
+numeric.
 
 
-// Check https://github.com/jpedro/tablelize/tree/master/examples.
+Usage
+
+Source:
+
+    package main
+    
+    import (
+      "github.com/jpedro/tablelize"
+    )
+    
+    func main() {
+      var data [][]string
+    
+      data = append(data, []string{"KEY", "VALUE", "NUMBER", "ALMOST_NUMBER"})
+      data = append(data, []string{"char", "a", "1", "1"})
+      data = append(data, []string{"longer-key-name", "Some text", "-2", "2"})
+      data = append(data, []string{"key", "And now for something completely different", "3", "3a"})
+    
+      tablelize.Rows(data)
+    }
+
+Output:
+
+    % go run examples/main.go
+    KEY               VALUE                                        NUMBER   ALMOST_NUMBER
+    char              a                                                 1   1
+    longer-key-name   Some text                                        -2   2
+    key               And now for something completely different        3   3a
+
+
+Check https://github.com/jpedro/tablelize/tree/master/examples.
+*/
 package tablelize
